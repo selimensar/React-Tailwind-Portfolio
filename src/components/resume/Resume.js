76c096c3e739d3473dc55485e2578bf2d3f960d0
Selim Ensar Koksal <selimensarkoksal@gmail.com>
2023-09-09 14:08:44 -0700

    resume title updated

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -13,10 +13,9 @@ const Resume = () => {
   return (
     <section id="resume" className="w-full py-20 border-b-[1px] border-b-black">
       <div className="flex justify-center items-center text-center">
-        <Title title="MY ACADEMIC AND WORK EXPERIENCE" des="My Resume" />
-        ?(6){" "}
+        <Title title="MY ACADEMIC AND WORK EXPERIENCE" des="My Resume" />{" "}
       </div>
-      :
+
       <div>
         <ul className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
           <li
