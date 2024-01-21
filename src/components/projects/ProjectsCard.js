fa21f24816f27bb0b0261c12a0c3b525a9862989
Selim Ensar Koksal <61663584+selimensar@users.noreply.github.com>
2024-01-21 15:58:14 -0800

    Update ProjectsCard.js

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -6,13 +6,15 @@ const ProjectsCard = ({ title, des, src, githubLink, websiteLink }) => {
   return (
     <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
       <div className="w-full h-[80%] overflow-hidden rounded-lg">
-        <img
-          className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
-          src={src}
-          alt="src"
-        />
+        <a href={websiteLink} target="_blank" rel="noopener noreferrer">
+          <img
+            className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
+            src={src}
+            alt={title}
+          />
+        </a>
       </div>
-      <div className="w-full mt-5 flex flex-col  gap-6">
+      <div className="w-full mt-5 flex flex-col gap-6">
         <div>
           <div className="flex items-center justify-between">
             <h3 className="text-base uppercase text-designColor font-normal">
