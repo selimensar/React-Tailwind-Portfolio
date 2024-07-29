fcc85cc9c07f0c748c2c53b67866e69a0bc64902
Selim Ensar Koksal <61663584+selimensar@users.noreply.github.com>
2024-07-28 20:36:21 -0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -39,47 +39,5 @@ export {
   sunriver,
   myNatours,
   myNexter,
-  import logo from "./images/logo.png";
-import bannerImage from "./images/bannerImage.jpeg";
-import moreleasmeadow from "./images/moreleasmeadow.jpeg";
-import projectOne from "./images/projects/projectOne.png";
-import projectTwo from "./images/projects/projectTwo.png";
-import projectThree from "./images/projects/projectThree.png";
-import projectFour from "./images/projects/projectFour.png";
-import projectFive from "./images/projects/projectFive.png";
-import projectSix from "./images/projects/projectSix.png";
-import projectSeven from "./images/projects/projectSeven.png";
-import sunriver from "./images/projects/sunriver.png";
-import contactImg from "./images/contact/contactImg.png";
-import projectEight from "./images/projects/projectEight.png";
-import projectNine from "./images/projects/projectNine.png";
-import projectTen from "./images/projects/projectTen.png";
-import projectEleven from "./images/projects/projectEleven.png";
-import projectTwelve from "./images/projects/projectTwelve.png";
-import myNatours from "./images/projects/my-natours.png"; 
-import myNexter from "./images/projects/my-nexter.png"; 
-import myTrillo from "./images/projects/my-trillo.png"; 
-
-export {
-  logo,
-  bannerImage,
-  projectOne,
-  projectTwo,
-  projectThree,
-  projectFour,
-  projectFive,
-  projectSix,
-  projectSeven,
-  projectEight,
-  projectNine,
-  projectTen,
-  projectEleven,
-  projectTwelve,
-  contactImg,
-  moreleasmeadow,
-  sunriver,
-  myNatours,
-  myNexter,
   myTrillo,
-};
 };
